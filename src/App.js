663dbd188e9f82107aa0933d3c7ad4eb5dffa42e
Francisco Barafani <franbarafani@gmail.com>
2020-08-19 17:07:28 -0300

    Libraries and Frameworks 2
    -Added React Router Dom
    -Added Main Layout and Admin Layout Folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+//Components
+import { Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">MasqueRogueApp</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/"></Route>
+          <Route path="/admin"></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
