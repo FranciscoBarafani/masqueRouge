b3134755a6e47db5489dbb276a4124b543602178
Francisco Barafani <franbarafani@gmail.com>
2020-08-19 17:00:34 -0300

    Libraries and Frameworks
    -Added Async
    -Added Firebase
    -Added Material-UI
    -Added Lodash
    -Installed Firebase Config
    -Created Main Project Folders

---
+++ src/utils/Firebase.js
@@ -0,0 +1,14 @@
+//Firebase Import
+import firebase from "firebase/app";
+//Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyA0r4IUeMI6Qau82nPt7EqPiW2pBAu-HWo",
+  authDomain: "masquerouge-88c27.firebaseapp.com",
+  databaseURL: "https://masquerouge-88c27.firebaseio.com",
+  projectId: "masquerouge-88c27",
+  storageBucket: "masquerouge-88c27.appspot.com",
+  messagingSenderId: "546091894210",
+  appId: "1:546091894210:web:5d433a5f320aad8b2c825b",
+};
+//Firebase Initialize
+export default firebase.initializeApp(firebaseConfig);
