23ea088384b97d8e1193f78a81106efaac995ef3
Francisco Barafani <franbarafani@gmail.com>
2020-11-03 22:17:51 -0300

    -Added Documents
    -Added Release 1 Notes
    -Added Release 2 Plan
    -Added Test Cases

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,7 @@ import firebase from "../../utils/Firebase";
 import "firebase/storage";
 
 export default function Card(props) {
-  const { title, description, picture, id,  addToCart } = props;
+  const { title, description, picture, id, addToCart } = props;
   const [image, setImage] = useState(null);
 
   const storageRef = firebase.storage().ref();
@@ -28,37 +28,42 @@ export default function Card(props) {
 
   //Create Product to add to Cart
   const addProduct = () => {
-
     const product = {
       id: id,
       name: description,
       price: title,
       picture: image,
       quantity: 1,
-    }
+    };
     addToCart(product, id);
-  }
+  };
 
   return (
     <div className="card">
-        <MyCard
-          hoverable
-          style={{ width: 240, height: 320 }}
-          actions={[
-            <Button onClick={() => addProduct()}>Agregar al Carrito</Button>
-          ]}
-          cover={
-            <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <MyCard
+        hoverable
+        style={{ width: 240, height: 320 }}
+        actions={[
+          <Button onClick={() => addProduct()}>Agregar al Carrito</Button>,
+        ]}
+        cover={
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <img
               alt={description}
               src={image}
               style={{ maxWidth: 220, height: 160 }}
             />
-            </div>
-          }
-        >
-          <MyCard.Meta title={`$${title}`} description={description} />
-        </MyCard>
+          </div>
+        }
+      >
+        <MyCard.Meta title={`$${title}`} description={description} />
+      </MyCard>
     </div>
   );
 }
